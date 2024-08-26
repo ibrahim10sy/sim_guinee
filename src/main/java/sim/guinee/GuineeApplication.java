87688d551153c7275628c60c0ed2ac340d2170a6
ibrahim10sy <ibrahim10sy@gmail.com>
2024-08-26 17:12:08 +0000

    commit

---
+++ src/main/java/sim/guinee/GuineeApplication.java
@@ -0,0 +1,13 @@
+package sim.guinee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuineeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sim/guinee/GuineeApplicationTests.java
@@ -0,0 +1,13 @@
+package sim.guinee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
